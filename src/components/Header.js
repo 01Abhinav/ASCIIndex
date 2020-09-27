cf0c9dabdc27284946d8e999f3e4214274eee67c
Abhinav raj <abhinav.zimmy@gmail.com>
2020-09-27 18:29:51 +0530

    Responsive header (#4)
    * Added header
    
    * responsive header logo
    
    Co-authored-by: Snehil <51155558+SneakySensei@users.noreply.github.com>

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,13 @@ import styled from 'styled-components';
 import Logo_image from '../logo.svg';
 
 const Logo =styled.div`
-width: 526px;
-margin: 0 auto;
 position: relative;
+img {
+    width: 80vw;
+    max-width:526px;
+    margin: 0 auto;
+    display: block;
+}
 `;
 
 
