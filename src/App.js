5d8a068efcc8295abc0b00fd41a3e534f1c4eb78
Abhinav <abhinav.zimmy@gmail.com>
2020-09-27 18:12:46 +0530

    added card container to app.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header'
+import CardsContainer from './components/CardsContainer'
 
 function App() {
   return (
    <div>
      <Header/>
+     <CardsContainer/>
    </div>
   );
 }
